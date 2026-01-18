9b47caf2c1945ae1583c93d2c13cacf2b5da9c15
Jon-Luca Bernardo <jonluca@Jon-Lucas-MacBook-Air.local>
2026-01-17 19:45:21 -0500

    fixed issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-<<<<<<< Updated upstream
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-=======
       '/api': 'http://localhost:8000',
       '/health': 'http://localhost:8000',
->>>>>>> Stashed changes
     },
   },
 })
-
